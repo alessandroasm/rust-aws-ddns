5af6446ae65b907d1f07ab8f9e8e9ae362ef923b
Alessandro Menezes <alessandroasm@gmail.com>
2020-11-04 00:53:24 -0400

    Version 0.1.2
    Bumping version to 0.1.2.
    
    Project dependencies were also updated and cross compilation target was changed
    to release.

--- cross-build.sh
+++ cross-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ./docker/build-container-armhf.sh
-cross build --target armv7-unknown-linux-gnueabihf --features openssl
+cross build --target armv7-unknown-linux-gnueabihf --features openssl --release
+
